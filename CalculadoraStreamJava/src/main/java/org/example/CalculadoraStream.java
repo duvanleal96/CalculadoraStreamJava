cc1fd9938f8bb9b39e1cdab76f7971587faf0a80
duvanleal96 <duvanleal65@gmail.com>
2022-07-07 22:38:26 -0500

    se agregan metodos de multiplicacion y division se agrega el filter para la division y el min para mostrar el minimo de los arreglos

--- CalculadoraStreamJava/src/main/java/org/example/CalculadoraStream.java
+++ CalculadoraStreamJava/src/main/java/org/example/CalculadoraStream.java
@@ -1,29 +1,47 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 public class CalculadoraStream {
 
 
-    public void sumar(List<Integer> valor1,List<Integer>valor2) {
+    public void sumar(List<Double> valor1,List<Double>valor2) {
           valor1.stream()
                 .map(number->{
                     int contador = valor1.indexOf(number);
                     return number + valor2.get(contador);
-                }).forEach(System.out::println);
+                }).forEach(e -> System.out.println("La suma es : " + e));
     }
-    public void resta(List<Integer> valor1,List<Integer>valor2) {
+    public void resta(List<Double> valor1,List<Double>valor2) {
         valor1.stream()
                 .map(number->{
                     int contador = valor1.indexOf(number);
                     return number - valor2.get(contador);
-                }).forEach(System.out::println);
+                }).forEach(e -> System.out.println("La resta es : " + e));
     }
-
+    public void multiplicacion(List<Double> valor1,List<Double>valor2) {
+        valor1.stream()
+                .map(number->{
+                    int contador = valor1.indexOf(number);
+                    return number * valor2.get(contador);
+                }).forEach(e -> System.out.println("La multiplicacion es : " + e));
+    }
+    public void division(List<Double> valor1,List<Double>valor2) {
+        valor1.stream()
+                .filter(e -> e > 0)
+                .map(number->{
+                    int contador = valor1.indexOf(number);
+                    return number / valor2.get(contador);
+                }).forEach(e -> System.out.println("la division : " + e));
+    }
+    public void minimo(List<Double> valor1,List<Double>valor2){
+        valor1.stream()
+                .min((i, j) -> i.compareTo(j))
+                .ifPresent(e->System.out.println("el numero minimo del arreglo 1 es "+ e));
+        valor2.stream()
+                .min((i, j) -> i.compareTo(j))
+                .ifPresent(e->System.out.println("el numero minimo del arreglo 2 es "+ e));
+    }
 
 }
 
--- CalculadoraStreamJava/src/main/java/org/example/Main.java
+++ CalculadoraStreamJava/src/main/java/org/example/Main.java
@@ -6,10 +6,13 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> valor1= Arrays.asList(1,2,3,4);
-        List<Integer> valor2= Arrays.asList(2,5,7,8);
+        List<Double> valor1= Arrays.asList(0.0,2.0,3.0,4.0);
+        List<Double> valor2= Arrays.asList(2.0,5.0,7.0,8.0);
         CalculadoraStream calcular = new CalculadoraStream();
         calcular.sumar(valor1,valor2);
         calcular.resta(valor1,valor2);
+        calcular.multiplicacion(valor1,valor2);
+        calcular.division(valor1,valor2);
+        calcular.minimo(valor1,valor2);
     }
 }
